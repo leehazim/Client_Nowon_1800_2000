798f831bcd302af13024724dbe9d5927a33c803a
seunghuyn <101630668+leehazim@users.noreply.github.com>
2022-04-19 21:57:01 +0900

    Template

---
+++ Client_SecondProject/Client_08_Template/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+template<typename T> void Swap(T& a, T& b) {
+	T tmp = a; a = b; b = tmp;
+}
+
+int main(void) {
+
+	int a = 3, b = 4; Swap(a, b); cout << a << ","<< b << endl;
+	double c = 1.2, d = 3.4; Swap(c, d); cout << c << "," << d << endl;
+	float e = 1.4f, f = 3.1f; 
+	Swap<float>(e, f); cout << e << "," << f << endl; 
+	return 0;
+}
